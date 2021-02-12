bb76a0f776f3f99e2d9d65f2768ded183bde217f
fundacicek <fun.cicek@gmail.com>
2021-02-12 21:18:35 +0300

    added final brach paralel excetuion

--- src/test/java/com/vytrack/step_definitions/Hooks.java
+++ src/test/java/com/vytrack/step_definitions/Hooks.java
@@ -8,17 +8,16 @@ import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 
+import java.util.concurrent.TimeUnit;
+
 public class Hooks {
 
     @Before
     public void setUp(){
         System.out.println("\tthis is coming from BEFORE");
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.get().manage().window().maximize();
     }
-
-//    @After
-//    public void tearDown(){
-//        Driver.closeDriver();
-//    }
 
 
     @After
@@ -32,16 +31,16 @@ public class Hooks {
 
     }
 
-//    @Before("@db")
-//    public void setUpdb(){
-//        System.out.println("\tconnecting to database...");
-//    }
-//
-//    @After("@db")
-//    public void closeDb(){
-//        System.out.println("\tdisconnecting to database...");
-//
-//    }
+    @Before("@db")
+    public void setUpdb(){
+        System.out.println("\tconnecting to database...");
+    }
+
+    @After("@db")
+    public void closeDb(){
+        System.out.println("\tdisconnecting to database...");
+
+    }
 
 
 
--- target/test-classes/features/LoginWithParameters.feature
+++ target/test-classes/features/LoginWithParameters.feature
@@ -6,7 +6,7 @@ Feature: Login as different users
     Then the user should be able to login
     And the title contains "Dashboard"
 
-
+@login
   Scenario: login as a driver
     Given the user logged in as "usertype"
     Then the user should be able to login
